e74751c4090a3e4e9a5b69d95e79372f04395ac2
Arash Mousavi <mousavi.arash@gmail.com>
2017-07-13 01:08:40 +0430

    Add user model

---
+++ lib/phoenix_toggl/web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixToggl.Web.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use PhoenixToggl.Web, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(PhoenixToggl.Web.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(PhoenixToggl.Web.ErrorView, :"404")
+  end
+end
---
+++ priv/repo/migrations/20170713083817_create_accounts_user.exs
@@ -0,0 +1,15 @@
+defmodule PhoenixToggl.Repo.Migrations.CreatePhoenixToggl.Accounts.User do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts_users) do
+      add :firstname, :string, null: false
+      add :lastname, :string
+      add :email, :string, null: false
+      add :encrypted_password, :string, null: false
+
+      timestamps()
+    end
+
+  end
+end
