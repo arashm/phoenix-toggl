464d29252edf189d7bdc0531babdcfb31baee36e
Arash Mousavi <mousavi.arash@gmail.com>
2017-07-16 00:02:40 +0430

    Add session view

---
+++ lib/phoenix_toggl/web/views/session_view.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixToggl.Web.SessionView do
+  use PhoenixToggl.Web, :view
+
+  def render("show.json", %{jwt: jwt, user: user}) do
+    %{
+      jwt: jwt,
+      user: user
+    }
+  end
+
+  def render("error.json", _) do
+    %{error: "Invalid email or password"}
+  end
+
+  def render("delete.json", _) do
+    %{ok: true}
+  end
+end
